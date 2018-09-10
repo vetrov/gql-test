13b2709d1cc56e3dceec23a25c1d6dd1da1068ed
Aleksandr Vetrov <aleksandr.vetrov@frosmo.com>
2018-09-10 17:21:38 +0300

    Added message addition and removal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4';
+
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
@@ -14,6 +16,11 @@ const schema = gql`
 
         messages: [Message!]!
         message(id: ID!): Message!
+    }
+
+    type Mutation {
+        createMessage(text: String!): Message!
+        deleteMessage(id: ID!): Boolean!
     }
 
     type User {
@@ -86,6 +93,30 @@ const resolvers = {
         },
         message: (parent, { id }) => {
             return messages[id];
+        },
+    },
+    Mutation: {
+        createMessage: (parent, { text }, { me }) => {
+            const id = uuidv4();
+            const message = {
+                id,
+                text,
+                userId: me.id,
+            };
+            messages[id] = message;
+            users[me.id].messageIds.push(id);
+
+            return message;
+        },
+        deleteMessage: (parent, { id }, { me }) => {
+            const { [id]: message, ...otherMessages } = messages;
+            if (!message) {
+                return false;
+            }
+
+            messages = otherMessages;
+
+            return true;
         },
     },
     User: {
